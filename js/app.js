ff130b79deebb00cb51fc799477fa39adf7218bc
huzaifafasih40c-collab <huzaifafasih40c@gmail.com>
2026-01-25 05:40:07 -0800

    js

---
+++ js/app.js
@@ -0,0 +1,180 @@
+// var varstring = "huzaifa"
+// var varnumber = 12000
+// alert("huzaifa")
+// var varboolean = true
+// var varnull = null
+// var varundefined = undefined
+// var vararray = ["asdf", 123, true];
+// var varabject ={ a: "abc"};
+// var varstring ="how are you?"
+// var varused = varnull
+// var a = 1;
+// console.log(++a)
+// console.log(a)
+// var a = "huzaifa"
+// var b = "fasih"
+
+
+// var a = "a";
+// var b = "2"; //Number like string
+// var c = a - b;
+// var c = a - " " - b; // concatenation
+// console.log(c)
+//  premitive datatypes
+// non premitive datatypes
+// console.log(fullname)
+// dynamic typed language
+// var varstring = 600
+// alert(fullname)
+// var 12alpha: not allowed
+// var al12pha: allowed
+// var my name: not allowed
+// var my*/name: not allowed
+// var my$name: allowed
+// var my%name:not llowed
+// var myname: allowed
+// var first-Full-Name: snake-Case
+// var FirstFullName: PascalCase
+// var firstFullName:
+
+// var a = "2";
+// var b = "2";
+// var c = b + a;
+// console.log(c)
+
+// var a = 5 + "3";
+// var b = "1" + 3 + 5 + "5";
+// console.log(a)
+// console.log(b)
+
+// var userAge = prompt ("enter your name");
+// var userUpdateAge = Number(userage + 6);
+// console.log(userUpdateAge, userage);
+
+// var a = "25.5";
+// // var b = Number(a);
+// var b = parseInt (a);
+// var b = parseFloat (a);
+// console.log(a, b);
+
+// var message = "your age is " + userage;
+// alert(message)
+
+// var rotiwala = true;
+// var parathawala = false;
+// if (rotiwala) {
+//     console.log("roti le ana");
+// console.log("double roti le ana");
+// }
+
+// var charges =prompt("Enter your charges");
+// var lowpayingclient = charges === 250;
+// // console.log(lowPayingClient);
+
+// var newCharges = lowPayingClient + 50;
+
+// var a = "40"
+// var b = 50
+
+// var comparision = a == b
+// var comparision = a > b
+// console.log(comparision)
+
+// var studentMarks = 79
+// var isStudentAPlus = studentMarks >= 80
+// var isStudentFailed = studentmarks >=39
+
+// var lowPayingClient = 250
+// var midPayingClient = 750
+// var highPayingClient = 1250
+// var charges = +prompt("Enter the charges")
+
+// var isLowpayingClient = charges <= lowPayingClient;
+// var isMidPayingClient = charges <= midPayingClient;
+
+// var tip = 0;
+// if (isLowPayingClient) {
+//     tip = (charges / 100) * 10;
+// } else if (isMidPayingClient) {
+//     tip = (charges / 100) * 15;
+// } else {
+//     tip = 500;
+// }
+// alert("Tip is " + tip);
+
+var userEmail = "huzaifafasih40c@gmail.com";
+var userpwd = "password@123";
+
+var email = prompt("Enter your email");
+var pwd = prompt("Enter your password");
+
+// if (email == userEmail) {
+//     if (pwd == userPwd) {
+//         alert("Login successfully");
+//     } else {
+//         alert("Credential not found");
+//     }
+// } else {
+//     alert("Credential not found");
+
+// }
+
+// if (email === userEmail && pwd === userPwd) {
+//     alert("User Verified");
+// }
+
+//     var isAlive = true
+//     var isAble = true
+//     var isRich = true
+//     var isBeautiful = false
+
+//     if(isAlive || isAble || isRich || isBeautiful) {
+//         console.log("do Marri")
+//     }
+
+//     var isRich = true
+//     var isHandsome = true
+   
+//     if( isRich && isHandsome && isCaring && isBusinessman) {
+//         console.log("====") var isCaring = true
+//     var isBusinessman = true
+    // }
+
+    var firstname = prompt("Enter The Name");
+    var lastname = prompt("Enter Last Name");
+
+    if (firstname == "HUZI") {
+        if (lastname == "light")
+        alert("WELCOME");
+    }
+    else if(firstname == "REHAN") {
+        if (lastname == light);
+        alert("WELCOME FREIND")
+    } else {
+        alert("HI")
+    }
+
+
+
+    // const students = ["HAMZA","SAQIB","AQIL","LAIQ"];
+    // const removedstudentname = students.shift();
+    // alert("removed student: " + removedstudentname);
+
+    // const totalstudents = students.unshift("REHAN","HAMZA");
+
+    // const indexnumber = 1
+    // console.log(students);
+
+    // const pushreturn = students.push("rehan", "Absent")
+    // const popReturn = students.pop();
+    // students.pop();
+
+    // students.shift();
+    // students.unshift("AllahWala");
+
+    // const A = [1,2,3];
+    // // const B = [1,2,3];
+    // console.log(A)
+
+
+    
